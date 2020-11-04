d71428ba415b34f2d5b9523da944ca92b25904f3
Marsel Khalikov <h096hk@gmail.com>
2020-11-04 21:34:41 +0300

    Market

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -65,6 +65,17 @@ class NewPlaceViewController: UITableViewController {
             view.endEditing(true)
         }
     }
+    
+    // MARK: Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "showMap" {
+            return
+        }
+        
+        let mapVC = segue.destination as! MapViewController
+        mapVC.place = currentPlace
+    }
 
     func savePlace() {
         
